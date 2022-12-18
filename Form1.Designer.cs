adcb53d0d3289e0406d517c76b31fc897cedb552
mahmut yavuz <live53123@hotmail.com>
2022-12-18 17:12:31 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace MayinTarlasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(23, 26);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(631, 398);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 442);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MayinTarlasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            // Usual MineField game
+            InitializeComponent();
+           
+            int counter = 0;
+            int safeCount = 0;
+            List<int> mayin = new List<int>();
+
+            Random rnd = new Random();
+            while (mayin.Count < 10)
+            {
+                
+                int may = rnd.Next(0, 54);
+                if (!mayin.Contains(may))
+                {
+                    mayin.Add(may);
+                }
+                //Mayınların hangi sayıdaki butonlarda olması gerektiğini belirleyen kısım
+            }
+            for (int i = 0; i < 54; i++)
+            {
+                // Button creation area
+                Button button1 = new Button();
+                button1.Location = new System.Drawing.Point(3, 3);
+                button1.Name = "*";
+                button1.Size = new System.Drawing.Size(42, 45);
+                button1.TabIndex = i;
+                button1.TabStop = false; // Makes the tab order disable
+                button1.Image = Image.FromFile(@"C:\Users\MAHMUT\Desktop\may.png");
+                button1.Tag = null;
+                button1.Click += Bttn_Clilck;
+                button1.Text = "*";
+                button1.UseVisualStyleBackColor = true;
+                flowLayoutPanel1.Controls.Add(button1);
+
+
+                if (mayin.Contains(i))
+                { // Mine creation area
+                    button1.Tag = 1;
+                }
+            }
+
+            void Bttn_Clilck(object sender, EventArgs e)
+            { //  Button clicked events
+                Button cilcked = (Button)sender;
+                object o = cilcked.Tag;
+                if (o != null)
+                { // if the clicked button is a mine this progress will  be executed
+                    cilcked.BackColor = Color.Red;
+                    cilcked.Enabled = false;
+                    Console.Beep(1500, 400);
+                    counter++;
+                }
+                else
+                { // if clicked button is not a mine this progress will be executed
+                    safeCount++;
+                    cilcked.Enabled = false;
+                }
+
+                if (counter == 3)
+                {// if player clicks 3 mine game overs
+                    MessageBox.Show("YOU LOST :(");
+                    this.Close();
+                }
+                else if (safeCount == 46)
+                {
+                    // if player cilcks less than 3 mine and all clear buttons are clicked player wins
+                    MessageBox.Show("YOU WON !");
+                }
+            }
+        }
+    }
+}
